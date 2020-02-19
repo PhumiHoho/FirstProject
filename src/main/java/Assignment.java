c23b3dac7b35402d4b5124185636589113abf232
Phumihoho <phumihoho@gmail.com>
2020-02-19 16:01:11 +0200

    upload assigment

---
+++ src/main/java/Assignment.java
@@ -0,0 +1,10 @@
+package assignment;
+
+/**
+ * Created by phumelela on 2/19/2020.
+ */
+public class Assignment {
+    public static void main(String[] args) {
+        System.out.println("Something");
+    }
+}
